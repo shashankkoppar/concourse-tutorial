d0a0e917525e7d530c4a0d7029e604cb275c4de7
Concourse <nobody@concourse-ci.org>
2019-06-17 13:58:17 +0200

    update gcpbuild

--- tutorials/basic/basic-pipeline/gcloudbuild.sh
+++ tutorials/basic/basic-pipeline/gcloudbuild.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 env
 set -eu
+cat /Users/shashank/Downloads/sre-tooling-5add44bce09f.json
+cat ${GCLOUD_SERVICE_ACCOUNT_KEY_FILE}
 echo "${GCLOUD_SERVICE_ACCOUNT_KEY_FILE}" >> sa.json
 gcloud auth activate-service-account --key-file=sa.json
 cd resource-tutorial/tutorials/miscellaneous/docker-images/docker
