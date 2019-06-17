9acb4af0d354134f9dd2c7259e6e50246de7401c
Concourse <nobody@concourse-ci.org>
2019-06-17 15:23:57 +0200

    delete deployment if existing

--- tutorials/basic/basic-pipeline/gcloudbuild.sh
+++ tutorials/basic/basic-pipeline/gcloudbuild.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-env
 set -eu
 
 echo "${GCLOUD_SERVICE_ACCOUNT_KEY_FILE}" >> sa.json
@@ -7,5 +6,5 @@ gcloud auth activate-service-account --key-file=sa.json
 cd resource-tutorial/tutorials/miscellaneous/docker-images/docker
 gcloud builds submit --tag=eu.gcr.io/sre-tooling/hello-world-example
 gcloud container clusters get-credentials tooling --zone europe-west1-d --project sre-tooling
-
+kubectl delete deployment hello-world
 kubectl run hello-world --image=eu.gcr.io/sre-tooling/hello-world-example
