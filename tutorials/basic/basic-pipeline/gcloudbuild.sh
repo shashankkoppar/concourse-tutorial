6e46ed1119db9021afc2c605f9162c5b4f370566
H Shashank Koppar <shashank.koppar1@gmail.com>
2019-06-17 15:38:32 +0200

    Update gcloudbuild.sh

--- tutorials/basic/basic-pipeline/gcloudbuild.sh
+++ tutorials/basic/basic-pipeline/gcloudbuild.sh
@@ -4,7 +4,7 @@ set -eu
 echo "${GCLOUD_SERVICE_ACCOUNT_KEY_FILE}" >> sa.json
 gcloud auth activate-service-account --key-file=sa.json
 cd resource-tutorial/tutorials/miscellaneous/docker-images/docker
-gcloud builds submit --tag=eu.gcr.io/sre-tooling/hello-world-example
+gcloud builds submit --tag=eu.gcr.io/sre-tooling/hello-world-example2
 gcloud container clusters get-credentials tooling --zone europe-west1-d --project sre-tooling
 kubectl delete deployment hello-world
-kubectl run hello-world --image=eu.gcr.io/sre-tooling/hello-world-example
+kubectl run hello-world --image=eu.gcr.io/sre-tooling/hello-world-example2
