e977f137d3262e0d29da5d3219452808ed164a73
Concourse <nobody@concourse-ci.org>
2019-06-18 15:01:33 +0200

    updatye script

--- tutorials/basic/basic-pipeline/gcloudbuild.sh
+++ tutorials/basic/basic-pipeline/gcloudbuild.sh
@@ -3,8 +3,8 @@ set -eu
 
 echo "${GCLOUD_SERVICE_ACCOUNT_KEY_FILE}" >> sa.json
 gcloud auth activate-service-account --key-file=sa.json
+gcloud config set builds/use_kaniko True
 cd resource-tutorial/tutorials/miscellaneous/docker-images/example-gradle
 gcloud builds submit --tag=eu.gcr.io/sre-tooling/gradle-example2 --no-cache
 gcloud container clusters get-credentials tooling --zone europe-west1-d --project sre-tooling
 kubectl apply -f deploy.yaml
-stern gradle-deployment --output=json --tail=100 | jq .
