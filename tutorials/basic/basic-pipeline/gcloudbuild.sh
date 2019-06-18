f9e11d9bec911fa6d604eabfa0f39a85a909db4c
Concourse <nobody@concourse-ci.org>
2019-06-18 14:41:12 +0200

    add gradle sample

--- tutorials/basic/basic-pipeline/gcloudbuild.sh
+++ tutorials/basic/basic-pipeline/gcloudbuild.sh
@@ -3,8 +3,7 @@ set -eu
 
 echo "${GCLOUD_SERVICE_ACCOUNT_KEY_FILE}" >> sa.json
 gcloud auth activate-service-account --key-file=sa.json
-cd resource-tutorial/tutorials/miscellaneous/docker-images/docker
-gcloud builds submit --tag=eu.gcr.io/sre-tooling/hello-world-example2
+cd resource-tutorial/tutorials/miscellaneous/docker-images/example-gradle
+gcloud builds submit --tag=eu.gcr.io/sre-tooling/gradle-example2 --no-cache
 gcloud container clusters get-credentials tooling --zone europe-west1-d --project sre-tooling
-kubectl delete deployment hello-world
-kubectl run hello-world --image=eu.gcr.io/sre-tooling/hello-world-example2
+kubectl apply -f deploy.yaml
