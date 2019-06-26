74986141ff342eb98531cae3c35b5d7e93e13ce2
Concourse <nobody@concourse-ci.org>
2019-06-26 15:19:13 +0200

    add project as well

--- tutorials/basic/basic-pipeline/gcloudbuild.sh
+++ tutorials/basic/basic-pipeline/gcloudbuild.sh
@@ -3,6 +3,7 @@ set -eu
 
 echo "${GCLOUD_SERVICE_ACCOUNT_KEY_FILE}" >> sa.json
 gcloud auth activate-service-account --key-file=sa.json
+gcloud config set project sre-tooling
 gcloud config set builds/use_kaniko True
 cd resource-tutorial/tutorials/miscellaneous/docker-images/example-gradle
 gcloud builds submit --tag=eu.gcr.io/sre-tooling/gradle-example2 --no-cache
