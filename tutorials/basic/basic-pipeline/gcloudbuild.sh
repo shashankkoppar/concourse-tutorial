3fc4d70cf701c1e2b5297e29c3b03c77e500c6bf
Concourse <nobody@concourse-ci.org>
2019-06-27 18:16:25 +0200

    kubectl

--- tutorials/basic/basic-pipeline/gcloudbuild.sh
+++ tutorials/basic/basic-pipeline/gcloudbuild.sh
@@ -6,9 +6,10 @@ gcloud auth activate-service-account --key-file=sa.json
 gcloud config set project sre-tooling
 gcloud config set builds/use_kaniko True
 cd resource-tutorial/tutorials/miscellaneous/docker-images/example-gradle
-gcloud builds submit --tag=eu.gcr.io/sre-tooling/gradle-example2 --no-cache
-gcloud components install kubectl
+gcloud builds submit --tag=eu.gcr.io/sre-tooling/gradle-example3 --no-cache
+CLOUDSDK_CORE_DISABLE_PROMPTS=1 gcloud components install kubectl
 gcloud container clusters get-credentials tooling --zone europe-west1-d --project sre-tooling
 CURRENT_CONTEXT=$(kubectl config current-context)
-kubernetes-deploy kaniko $CURRENT_CONTEXT --template-dir=.
-stern -n kaniko --exclude-container istio-proxy gradle-deployment
+kubernetes-deploy kaniko $CURRENT_CONTEXT  --template-dir=.
+
+stern -n kaniko --exclude-container istio-proxy
